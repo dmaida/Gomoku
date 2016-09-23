91e9a606c7b721ce321679c7f4c920c6eee3e8a5
Daniel Esteban Maida <daniel.maida@wsu.edu>
2016-09-23 13:36:45 -0700

    Driver: Daniel; Navigator: Nazar; Setup our Gomoku board. We are able to place moves, alternates between black and white succesfully. Need to check if the player won. Setup the basic outline for the view/controller.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    @FXML
+    private GridPane gp;
+
+    @FXML
+    private Button restart;
+
+    @FXML
+    private Button quitOne;
+
+    @FXML
+    private Button Back;
+
+    @FXML
+    private Button[][] buttonMatrix;
+
+    @FXML
+    private MenuButton system;
+
+    @FXML
+    private MenuItem changeBG;
+
+    @FXML
+    private MenuItem quitTwo;
+
+    @FXML
+    private void initialize() {
+
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,79 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public Cell[][] grid;
+    public boolean currentCOLOR = true; //TRUE-white, False-black
+
+    public void createBoard() {
+        grid = new Cell[15][15];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                Cell cell = new Cell();
+                cell.occupied = false;
+                grid[i][j] = cell;
+            }
+        }
+    }
+
+    public boolean winCheck() {
+        return false;
+    }
+
+    public void makeMove(int row, int col) {
+
+        Cell cell = grid[row][col];
+
+        if (!cell.occupied) {
+            cell.occupied = true;
+            cell.blackORWHITE = currentCOLOR;
+            currentCOLOR = !currentCOLOR; //change of turns
+        }
+        print();
+    }
+
+    private void print() {
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.println();
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j].occupied){
+                    if(!grid[i][j].blackORWHITE){
+                        System.out.print("|X|");
+                    }
+                    else{
+                        System.out.print("|O|");
+                    }
+                }
+                else{
+                    System.out.print("|_|");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public class Cell{
+        boolean occupied;
+        boolean blackORWHITE;
+
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Gomoku");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        Main m = new Main();
+        m.createBoard();
+        m.makeMove(5,5);
+        m.makeMove(5,6);
+        m.makeMove(5,7);
         launch(args);
     }
 }
