18f817f7ca10d168a01a87fb423e489312b32346
Nazar Leonidovich Stelmakh <nazar.stelmakh@wsu.edu>
2016-09-23 15:47:18 -0700

    Refactored a bit to make it more legible.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,10 +26,7 @@ public class Main extends Application {
 
     private boolean winner(int row, int col) {
         // This is called just after a piece has been played on the
-        // square in the specified row and column.  It determines
-        // whether that was a winning move by counting the number
-        // of squares in a line in each of the four possible
-        // directions from (row,col).  If there are 5 squares (or more)
+        // square in the specified row and column. If there are 5 squares (or more)
         // in a row in any direction, then the game is won.
 
         if (count(row, col, 1, 0 ) >= 5)
@@ -41,51 +38,36 @@ public class Main extends Application {
         if (count(row, col, 1, 1 ) >= 5)
             return true;
 
-          /* When we get to this point, we know that the game is not
-             won.  The value of win_r1, which was changed in the count()
-             method, has to be reset to -1, to avoid drawing a red line
-             on the board. */
+        // When we get to this point, we know that the game is not won. 
 
-        //win_r1 = -1;
         return false;
 
-    }  // end winner()
+    }
 
 
     private int count(int row, int col, int dirX, int dirY) {
 
 
-        int ct = 1;  // Number of pieces in a row belonging to the player.
+        int ct = 1;  // Holds current number of pieces in a 'row'
 
-        int r, c;    // A row and column to be examined
-
+        int r;    
+        int c;      // A row and column to be examined
+        
         r = row + dirX;  // Look at square in specified direction.
         c = col + dirY;
-        while ( r >= 0 && r < 15 && c >= 0 && c < 15 && grid[r][c].occupied &&grid[r][c].blackORWHITE == currentCOLOR ) {
-            // Square is on the board and contains one of the players's pieces.
+        while ( r >= 0 && r < 15 && c >= 0 && c < 15 && grid[r][c].occupied && grid[r][c].blackORWHITE == currentCOLOR ) {
             ct++;
             r += dirX;  // Go on to next square in this direction.
             c += dirY;
         }
 
-        //win_r1 = r - dirX;  // The next-to-last square looked at.
-        //win_c1 = c - dirY;  //    (The LAST one looked at was off the board or
-        //    did not contain one of the player's pieces.
-
         r = row - dirX;  // Look in the opposite direction.
         c = col - dirY;
         while ( r >= 0 && r < 15 && c >= 0 && c < 15 && grid[r][c].occupied && grid[r][c].blackORWHITE == currentCOLOR ) {
-            // Square is on the board and contains one of the players's pieces.
             ct++;
             r -= dirX;   // Go on to next square in this direction.
             c -= dirY;
         }
-
-        //win_r2 = r + dirX;
-        //win_c2 = c + dirY;
-
-        // At this point, (win_r1,win_c1) and (win_r2,win_c2) mark the endpoints
-        // of the line of pieces belonging to the player.
 
         return ct;
 
