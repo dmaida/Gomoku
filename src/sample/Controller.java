696622fe12a94e2f225dbb45bf885eeedbd54feb
Daniel Esteban Maida <daniel.maida@wsu.edu>
2016-09-26 15:38:06 -0700

    Driver: Daniel; Navigator: Nazar; all buttons are working. Still need to work on formatting the window and changing the background image.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,21 +6,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Controller {
-
-    private Game Boardgame;
-
-    private void runGame(){
-        Boardgame = new Game();
-        Boardgame.createBoard();
-        makeButtonMatrix();
-
-    }
 
     @FXML
     private Button[][] btnMatrix;
@@ -46,6 +41,16 @@ public class Controller {
 
     @FXML
     private MenuItem quitTwo;
+
+    private Game Boardgame;
+    private int BGimage;
+
+    private void runGame(){
+        Boardgame = new Game();
+        Boardgame.createBoard();
+        makeButtonMatrix();
+
+    }
 
     private void giveEachBtnAnAction(int i, int j){
         final int currRow = i;
@@ -96,6 +101,7 @@ public class Controller {
                 Button btn = new Button();
                 btn.setMaxSize(30, 30);
                 btn.setMinSize(30, 30);
+                btn.setStyle("-fx-background-color: lightgray");
                 btnMatrix[i][j] = btn;
                 gp.add(btn, j, i);
 
@@ -113,7 +119,24 @@ public class Controller {
     @FXML
     private void initialize() {
 
+        BGimage = 0;
         runGame();
+
+        changeBG.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (BGimage == 0){
+                    //SET BACKGROUND IMAGES
+                    BGimage++;
+                }
+                if (BGimage == 1){
+                    BGimage++;
+                }
+                if (BGimage == 2){
+                    BGimage = 0;
+                }
+            }
+        });
 
         restart.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -131,6 +154,17 @@ public class Controller {
             @Override
             public void handle(ActionEvent event) {
                 System.exit(0);
+            }
+        });
+        Back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(Boardgame.numTurns>0) {
+                    int row = Boardgame.gameHistory[Boardgame.numTurns - 1].row;
+                    int col = Boardgame.gameHistory[Boardgame.numTurns - 1].col;
+                    btnMatrix[row][col].setStyle("-fx-background-color: lightgray");
+                    Boardgame.undoMove();
+                }
             }
         });
 
--- src/sample/Game.java
+++ src/sample/Game.java
@@ -7,22 +7,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Pair;
+
+import java.util.AbstractQueue;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Game extends Application {
 
     public Cell[][] grid;
     public boolean currentCOLOR = true; //TRUE-white, False-black
     public boolean endOfGame;
-    Stage GameWindow;
+    public Cell[] gameHistory;
+    public int numTurns;
+    public Parent gameWindow;
+
 
     public void createBoard() {
         grid = new Cell[15][15];
+        gameHistory = new Cell[255];
+        numTurns = 0;
         endOfGame = false;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 Cell cell = new Cell();
                 cell.occupied = false;
+                cell.col = -1;
+                cell.row = -1;
                 grid[i][j] = cell;
             }
         }
@@ -43,7 +57,7 @@ public class Game extends Application {
         if (count(row, col, 1, 1 ) >= 5)
             return true;
 
-        // When we get to this point, we know that the game is not won. 
+        // When we get to this point, we know that the game is not won.
 
         return false;
 
@@ -55,9 +69,9 @@ public class Game extends Application {
 
         int ct = 1;  // Holds current number of pieces in a 'row'
 
-        int r;    
+        int r;
         int c;      // A row and column to be examined
-        
+
         r = row + dirX;  // Look at square in specified direction.
         c = col + dirY;
         while ( r >= 0 && r < 15 && c >= 0 && c < 15 && grid[r][c].occupied && grid[r][c].blackORWHITE == currentCOLOR ) {
@@ -78,7 +92,7 @@ public class Game extends Application {
 
     }
 
-    
+
 
     public boolean makeMove(int row, int col) {
         Cell cell = grid[row][col];
@@ -86,8 +100,13 @@ public class Game extends Application {
         if (!cell.occupied && !endOfGame) {
             cell.occupied = true;
             cell.blackORWHITE = currentCOLOR;
+            cell.row = row;
+            cell.col = col;
+            gameHistory[numTurns] = cell;
+            numTurns++;
 
             print();
+            printHistory();
 
             if(winner(row, col)){
                 endOfGame = true;
@@ -100,6 +119,21 @@ public class Game extends Application {
 
         else return false;
 
+    }
+
+    public void undoMove(){
+        if(numTurns>0) {
+            numTurns--;
+            Cell cell = grid[gameHistory[numTurns].row][gameHistory[numTurns].col];
+            cell.occupied = false;
+            print();
+        }
+    }
+
+    private void printHistory(){
+        for (int i = 0; i < numTurns; i++) {
+            System.out.print("["+gameHistory[i].row+", "+ gameHistory[i].col+"]");
+        }
     }
 
     private void print() {
@@ -126,6 +160,8 @@ public class Game extends Application {
     public class Cell{
         boolean occupied;
         boolean blackORWHITE;
+        int row;
+        int col;
 
     }
 
@@ -136,7 +172,7 @@ public class Game extends Application {
         primaryStage.setTitle("Gomoku");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
-        GameWindow = primaryStage;
+        gameWindow = root;
     }
 
 
